ecd671e0cd46edf7ba048b55f174a8ac51196e92
Santiago Soto <cristiansantiago.soto.cardona@gmail.com>
2024-10-31 23:58:19 -0400

    cf

--- Codeforces/Problemas/Justice_Served.cpp
+++ Codeforces/Problemas/Justice_Served.cpp
@@ -1,20 +1,15 @@
-//Gracias Lin <3
+//Super las explicaciones de Lin, q haría sin él
 #include <bits/stdc++.h>
 using namespace std;
 #define ll long long
 #define vl vector<ll>
-#define vs vector<string>
-#define vb vector<bool>
-#define ull unsigned long long
 #define pll pair<ll, ll>
 #define pb push_back
 #define fi first
 #define se second
 #define ff(i, p, x) for (ll i = p; i < x; i++)
 #define all(a) (a).begin(), (a).end()
-#define ed "\n"
 #define IO std::ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
-ll MOD = 1e9+7;
 
 int main(){
 	IO
@@ -25,25 +20,26 @@ int main(){
 		ll a, b;
 		cin >> a >> b;
 		b += a;
+		b *= -1;
 		nums[i] = {{a, b}, i};
 	}
 	sort(all(nums));
-	vl limits(n, -1), tot(n, 0);
-	limits[0] = nums[0].fi.se;
+	vl limits(n, -1), tot(n, 0); // max de cada pos // respuesta
+	limits[0] = -nums[0].fi.se;
 	
 	ff(i, 1, n){
 		ll l = 0, r = n-1;
-		while(l != r){
-			ll mid = l + (r-l)/2;
-			if(nums[i].fi.se > limits[mid]){
+		while(l < r){
+			ll mid = l + (r - l) / 2;
+			if(-nums[i].fi.se <= limits[mid]){
+				l = mid + 1;
+			} else {
 				r = mid;
 			}
-			else{
-				l = mid+1;
-			}
 		}
-		tot[nums[i].se] = l;
-		limits[l] = nums[i].fi.se;
+		
+		tot[nums[i].se] = l; //valor en la pos i
+		limits[l] = -nums[i].fi.se; //base para verificar <= en el sig paso
 	}
 	
 	ff(i, 0, n){
