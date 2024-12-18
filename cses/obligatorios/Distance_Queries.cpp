c90442bd962e110505d3ec35ec60759e61308d16
Santiago Soto <cristiansantiago.soto.cardona@gmail.com>
2024-12-17 22:05:37 -0400

    cses

---
+++ cses/obligatorios/Distance_Queries.cpp
@@ -0,0 +1,90 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define vl vector<ll>
+#define vs vector<string>
+#define vb vector<bool>
+#define vc vector<char>
+#define ull unsigned long long
+#define pll pair<ll, ll>
+#define pb push_back
+#define fi first
+#define se second
+#define ff(i, p, x) for (ll i = p; i < x; i++)
+#define all(a) (a).begin(), (a).end()
+#define rall(a) (a).rbegin(), (a).rend()
+#define ed "\n"
+#define IO std::ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+ll MOD = 1e9+7;
+ 
+vector<vl> conexiones, ancestro;
+vl prof;
+ 
+void dfs(ll cur, ll par){
+	ancestro[cur][0] = par;
+	ff(i, 1, 20){
+		if(ancestro[cur][i-1] != -1){
+			ancestro[cur][i] = ancestro[ancestro[cur][i-1]][i-1];
+		}
+		else{
+			ancestro[cur][i] = -1;
+		}
+	}
+	for(ll child : conexiones[cur]){
+		if(child != par){
+			prof[child] = prof[cur]+1;
+			dfs(child, cur);
+		}
+	}
+}
+ 
+ll lca(ll a, ll b){
+	if(prof[a] < prof[b]){
+		swap(a, b);
+	}
+	ll dif = prof[a] - prof[b];
+	ff(i, 0, 20){
+		if(dif & (1 << i)){
+			a = ancestro[a][i];
+		}
+	}
+	if(a == b){
+		return a;
+	}
+	for(ll i = 19; i >= 0; i--){
+		if(ancestro[a][i] != ancestro[b][i]){
+			a = ancestro[a][i];
+			b = ancestro[b][i];
+		}
+	}
+	return ancestro[a][0];
+}
+ 
+int main(){
+	IO
+	ll n, q;
+	cin >> n >> q;
+	conexiones = vector<vl>(n);
+	ancestro = vector<vl>(n, vl(20, -1));
+	prof = vl(n, 0);
+	ff(i, 1, n){
+		ll a, b;
+		cin >> a >> b;
+		a--;
+		b--;
+		conexiones[a].pb(b);
+		conexiones[b].pb(a);
+	}
+	dfs(0, -1);
+	while(q--){
+		ll a, b;
+		cin >> a >> b;
+		a--;
+		b--;
+		ll x = lca(a, b);
+		cout << prof[a]+prof[b]-2*prof[x] << ed;
+	}
+	
+	return 0;
+}
+//con el de company queries II ya funciona todo
