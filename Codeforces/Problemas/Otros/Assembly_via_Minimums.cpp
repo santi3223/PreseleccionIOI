60018657a8a387ffe89e07c076e9b5e0e2059d44
Santiago Soto <cristiansantiago.soto.cardona@gmail.com>
2025-01-03 21:58:05 -0400

    problemas

---
+++ Codeforces/Problemas/Otros/Assembly_via_Minimums.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define vl vector<ll>
+#define vs vector<string>
+#define vb vector<bool>
+#define vc vector<char>
+#define ull unsigned long long
+#define pll pair<ll, ll>
+#define pb push_back
+#define fi first
+#define se second
+#define ff(i, p, x) for (ll i = p; i < x; i++)
+#define all(a) (a).begin(), (a).end()
+#define rall(a) (a).rbegin(), (a).rend()
+#define ed "\n"
+#define IO std::ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+ll MOD = 1e9+7;
+
+int main(){
+	IO
+	ll t;
+	cin >> t;
+	while(t--){
+		ll n;
+		cin >> n;
+		ll ori = n;
+		n *= n-1;
+		n /= 2;
+		vl arr(n);
+		ff(i, 0, n){
+			cin >> arr[i];
+		}
+		sort(all(arr));
+		ll x = ori-1, i = 0;
+		while(i < n){
+			cout << arr[i] << " " ;
+			i += x;
+			x--;
+		}
+		cout << "1000000000" << ed;
+	}
+	
+	return 0;
+	
+}
