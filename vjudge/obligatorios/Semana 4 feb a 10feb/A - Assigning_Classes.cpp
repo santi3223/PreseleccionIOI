3ed092bfc10b39db05360cbc4bcb8c4a4650aaea
Santiago Soto <cristiansantiago.soto.cardona@gmail.com>
2025-02-04 22:27:02 -0400

    vjudge

---
+++ vjudge/obligatorios/Semana 4 feb a 10feb/A - Assigning_Classes.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define vl vector<ll>
+#define vs vector<string>
+#define vb vector<bool>
+#define vc vector<char>
+#define ull unsigned long long
+#define pll pair<ll, ll>
+#define pb push_back
+#define fi first
+#define se second
+#define ff(i, p, x) for(ll i = p; i < x; i++)
+#define all(a) (a).begin(), (a).end()
+#define rall(a) (a).rbegin(), (a).rend()
+#define ed "\n"
+#define IO ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+ll MOD = 1e9+7;
+
+int main(){
+	IO
+	ll t;
+	cin >> t;
+	while(t--){
+		ll n;
+		cin >> n;
+		vl arr(2*n);
+		ff(i, 0, 2*n){
+			cin >> arr[i];
+		}
+		sort(all(arr));
+		cout << arr[(2*n-1)/2+1] - arr[(2*n-1)/2] << ed;
+	}
+	
+	return 0;
+}
