cf809d9c13ccce1d54131910c76965b96161a074
Santiago Soto <cristiansantiago.soto.cardona@gmail.com>
2025-02-27 23:38:19 -0400

    problemas

---
+++ Codeforces/Problemas/Otros/Traing_and_Queries.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define vl vector<ll>
+#define vs vector<string>
+#define vb vector<bool>
+#define vc vector<char>
+#define ull unsigned long long
+#define pll pair<ll, ll>
+#define pb push_back
+#define fi first
+#define se second
+#define ff(i, p, x) for(ll i = p; i < x; i++)
+#define all(a) (a).begin(), (a).end()
+#define rall(a) (a).rbegin(), (a).rend()
+#define ed "\n"
+#define IO ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+ll MOD = 1e9+7;
+ 
+int main(){
+	IO
+	ll t;
+	cin >> t;
+	while(t--){
+		ll n, k;
+		cin >> n >> k;
+		map<ll, pll> mp;
+		ff(i, 0, n){
+			ll u;
+			cin >> u;
+			if(!mp.count(u)){
+				mp[u].fi = i;
+			}
+			mp[u].se = i;
+		}
+		ff(i, 0, k){
+			ll a, b;
+			cin >> a >> b;
+			if(!mp.count(a) || !mp.count(b) || mp[a].fi > mp[b].se){
+				cout << "NO";
+			}
+			else{
+				cout << "YES";
+			}
+			cout << ed;
+		}
+	}
+    return 0;
+}
