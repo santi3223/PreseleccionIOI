29ee9a13110ba82445250102d144451cd7a37c20
Santiago Soto <cristiansantiago.soto.cardona@gmail.com>
2025-04-25 23:58:43 -0400

    problemas

---
+++ Codeforces/Problemas/Otros/Pythagorean_Triples.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define vl vector<ll>
+#define pb push_back
+#define pll pair<ll, ll>
+#define vs vector<string>
+#define vb vector<bool>
+#define all(aaa) aaa.begin(), aaa.end()
+#define rall(aaa) aaa.rbegin(), aaa.rend()
+#define ff(aa, bb, cc) for(ll aa = bb; aa < cc; aa++)
+#define IO ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define ed "\n"
+#define fi first
+#define se second
+ll MOD = 1e9+7;
+
+
+int main(){
+	ll t;
+	cin >> t;
+	while(t--){
+		ll n;
+		cin >> n;
+		ll c = 0;
+		for(ll i = 3; i*i <= 2*n-1; i += 2){
+			c++;
+		}
+		cout << c << ed;
+	}
+	
+	
+	return 0;
+}
