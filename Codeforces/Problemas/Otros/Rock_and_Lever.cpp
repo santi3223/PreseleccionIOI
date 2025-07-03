4572290c5ab06f64fd642123a62611393ce5b457
Santiago Soto <cristiansantiago.soto.cardona@gmail.com>
2025-07-02 22:26:20 -0400

    problemas

---
+++ Codeforces/Problemas/Otros/Rock_and_Lever.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define vl vector<ll>
+#define vb vector<bool>
+#define pb push_back
+#define ff(aa, bb, cc) for(ll aa = bb; aa < cc; aa++)
+#define pll pair<ll, ll>
+#define fi first
+#define se second
+#define ed "\n"
+#define all(aaa) aaa.begin(), aaa.end()
+#define rall(aaa) aaa.rbegin(), aaa.rend()
+ll MOD = 1e9+7;
+
+int main(){
+	ll t;
+	cin >> t;
+	while(t--){
+		ll n;
+		cin >> n;
+		vl arr(n);
+		ff(i, 0, n){
+			cin >> arr[i];
+		}
+		ll c = 0;
+		for(ll j = 29; j >= 0; j--){
+			ll cur = 0;
+			ff(i, 0, n){
+				if(arr[i] >= (1<<j) && arr[i]<(1<<(j+1))){
+					cur++;
+				}
+			}
+			c += cur*(cur-1)/2;
+		}
+		cout << c << ed;
+	}
+}
