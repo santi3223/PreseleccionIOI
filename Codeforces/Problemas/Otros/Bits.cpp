ff6a3c2244afebe2215227a6bab7953931d35724
Santiago Soto <cristiansantiago.soto.cardona@gmail.com>
2025-08-18 23:02:33 -0400

    problemas

---
+++ Codeforces/Problemas/Otros/Bits.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define vl vector<ll>
+#define ff(aa, bb, cc) for(ll aa = bb; aa < cc; aa++)
+#define ed "\n" 
+#define pb push_back
+#define all(aaa) aaa.begin(), aaa.end()
+#define rall(aaa) aaa.rbegin(), aaa.rend()
+
+ll f(ll l, ll r){
+	if(l == r){
+		return l;
+	}
+	ll num = log2(r);
+	if((1 << num) <= l){
+		return f(l-(1 << num), r-(1 << num))+ (1<<num);
+	}
+	else if((1 << (num+1))-1 <= r){
+		return (1 << (num+1))-1;
+	}
+	else{
+		return (1 << num)-1;
+	}
+}
+
+int main(){
+	ll t;
+	cin >> t;
+	while(t--){
+		ll l, r;
+		cin >> l >> r;
+		cout << f(l, r) << ed;
+	}
+}
