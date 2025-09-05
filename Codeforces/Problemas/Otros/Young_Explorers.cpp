0a26e21011722c85b2bf06602253f7172aff57e0
Santiago Soto <cristiansantiago.soto.cardona@gmail.com>
2025-09-04 23:26:04 -0400

    problemas

---
+++ Codeforces/Problemas/Otros/Young_Explorers.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define vl vector<ll>
+#define pll pair<ll, ll>
+#define fi first
+#define se second
+#define ff(aa, bb, cc) for(ll aa = bb; aa < (ll)cc; aa++)
+#define all(aaa) aaa.begin(), aaa.end()
+#define rall(aaa) aaa.rbegin(), aaa.rend()
+#define vb vector<bool>
+#define ed "\n"
+#define pb push_back
+
+int main(){
+	ll t;
+	cin >> t;
+	while(t--){
+		ll n;
+		cin >> n;
+		vl arr(n);
+		ff(i, 0, n){
+			cin >> arr[i];
+		}
+		sort(all(arr));
+		ll ans = 0, c = 0;
+		ff(i, 0, n){
+			if(++c == arr[i]){
+				ans++;
+				c = 0;
+			}
+		}
+		cout << ans << ed;
+	}
+	
+}
