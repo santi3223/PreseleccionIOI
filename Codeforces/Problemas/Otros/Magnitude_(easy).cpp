dee1b1dcc3ca93677a1bb575b5ac3af09ab0ea53
Santiago Soto <cristiansantiago.soto.cardona@gmail.com>
2025-09-09 23:16:30 -0400

    problemas

---
+++ Codeforces/Problemas/Otros/Magnitude_(easy).cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define vl vector<ll>
+#define pll pair<ll, ll>
+#define fi first
+#define se second
+#define ff(aa, bb, cc) for(ll aa = bb; aa < (ll)cc; aa++)
+#define all(aaa) aaa.begin(), aaa.end()
+#define rall(aaa) aaa.rbegin(), aaa.rend()
+#define vb vector<bool>
+#define ed "\n"
+#define pb push_back
+
+int main(){
+	ll t;
+	cin >> t;
+	while(t--){
+		ll n;
+		cin >> n;
+		vl arr(n);
+		ff(i, 0, n){
+			cin >> arr[i];
+		}
+		ll sum = 0, minn = 0;
+		ff(i, 0, n){
+			sum += arr[i];
+			minn = min(minn, sum);
+		}
+		cout << sum - 2*minn << ed;
+	}
+
+}
