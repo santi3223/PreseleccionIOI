acbd05460985f350b55a93ca50793c2f13a84a2f
Santiago Soto <cristiansantiago.soto.cardona@gmail.com>
2025-09-12 23:31:54 -0400

    problemas

---
+++ Codeforces/Problemas/Otros/Jumps.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define vl vector<ll>
+#define pll pair<ll, ll>
+#define fi first
+#define se second
+#define ff(aa, bb, cc) for(ll aa = bb; aa < (ll)cc; aa++)
+#define all(aaa) aaa.begin(), aaa.end()
+#define rall(aaa) aaa.rbegin(), aaa.rend()
+#define vb vector<bool>
+#define ed "\n"
+#define pb push_back
+ll MOD = 1e9+7;
+
+int main(){
+	ll t;
+	cin >> t;
+	while(t--){
+		ll x;
+		cin >> x;
+		ll c = 0;
+		while(c*(c+1) < 2*x){
+			c++;
+		}
+		if(c*(c+1)/2 == x+1){
+			c++;
+		}
+		cout << c << ed;
+	}
+
+}
