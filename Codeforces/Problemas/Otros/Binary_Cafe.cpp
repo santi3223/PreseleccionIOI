f4ae0b4bf95b3d83e72289764e5e2b875f3121a2
Santiago Soto <cristiansantiago.soto.cardona@gmail.com>
2025-09-14 23:56:20 -0400

    problemas

---
+++ Codeforces/Problemas/Otros/Binary_Cafe.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll int
+#define vl vector<ll>
+#define pll pair<ll, ll>
+#define fi first
+#define se second
+#define ff(aa, bb, cc) for(ll aa = bb; aa < (ll)cc; aa++)
+#define all(aaa) aaa.begin(), aaa.end()
+#define rall(aaa) aaa.rbegin(), aaa.rend()
+#define vb vector<bool>
+#define ed "\n"
+#define pb push_back
+ll MOD = 1e9+7;
+
+int main(){
+	ll t;
+	cin >> t;
+	while(t--){
+		ll n, k;
+		cin >> n >> k;
+		k = min(k, 30);
+		cout << min(n, (1<<k)-1)+1 << ed;
+	}
+
+}
