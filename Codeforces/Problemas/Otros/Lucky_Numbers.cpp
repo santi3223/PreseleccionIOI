fb88b126992641576d46ea6fa69d177ea08279cb
Santiago Soto <cristiansantiago.soto.cardona@gmail.com>
2025-10-26 22:45:52 -0400

    problemas

---
+++ Codeforces/Problemas/Otros/Lucky_Numbers.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define vl vector<ll>
+#define ff(aa, bb, cc) for(ll aa = bb; aa < cc; aa++)
+#define ed "\n"
+#define fi first
+#define se second
+#define pll pair<ll, ll>
+#define vb vector<bool>
+#define pb push_back
+#define all(aaa) aaa.begin(), aaa.end()
+#define rall(aaa) aaa.rbegin(), aaa.rend()
+ll MOD = 1e9+7;
+
+int main(){
+	ll n;
+	cin >> n;
+	ll c = 0;
+	ff(i, 1, n+1){
+		cout << c << ed;
+		c += (1 << i);
+	}
+	cout << c;
+}
+ 
